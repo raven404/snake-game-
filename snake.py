6e07762e7639ba721b5ca8b94585ccf268902119
raven404 <gky1232000@gmail.com>
2019-07-01 14:30:07 -0400

    class added

--- snake.py
+++ snake.py
@@ -17,3 +17,39 @@ def create_block():
     BLOCK = c.create_oval(posx, posy,
                           posx+SEG_SIZE, posy+SEG_SIZE,
                           fill="red")
+def main():
+    """ Handles game process """
+    global IN_GAME
+    if IN_GAME:
+        s.move()
+        head_coords = c.coords(s.segments[-1].instance)
+        x1, y1, x2, y2 = head_coords
+        # Check for collision with gamefield edges
+        if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
+            IN_GAME = False
+        # Eating apples
+        elif head_coords == c.coords(BLOCK):
+            s.add_segment()
+            c.delete(BLOCK)
+            create_block()
+        # Self-eating
+        else:
+            for index in range(len(s.segments)-1):
+                if head_coords == c.coords(s.segments[index].instance):
+                    IN_GAME = False
+        root.after(100, main)
+    # Not IN_GAME -> stop game and print message
+    else:
+        c.create_text(WIDTH/2, HEIGHT/2,
+                      text="GAME OVER!",
+                      font="Arial 20",
+                      fill="red")
+
+
+class Segment(object):
+    """ Single snake segment """
+    def __init__(self, x, y):
+        self.instance = c.create_rectangle(x, y,
+                                           x+SEG_SIZE, y+SEG_SIZE,
+                                           fill="white")
+
