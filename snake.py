a0b2a9234155b4dfbc91bceb0479584d393126c3
raven <32195921+raven404@users.noreply.github.com>
2019-07-01 14:18:04 -0400

    work started

---
+++ snake.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import random
+
+# Globals
+WIDTH = 800
+HEIGHT = 600
+SEG_SIZE = 20
+IN_GAME = True
+
+
+# Helper functions
+def create_block():
+    """ Creates an apple to be eaten """
+    global BLOCK
+    posx = SEG_SIZE * random.randint(1, (WIDTH-SEG_SIZE) / SEG_SIZE)
+    posy = SEG_SIZE * random.randint(1, (HEIGHT-SEG_SIZE) / SEG_SIZE)
+    BLOCK = c.create_oval(posx, posy,
+                          posx+SEG_SIZE, posy+SEG_SIZE,
+                          fill="red")
