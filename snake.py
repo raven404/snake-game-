196fa34e8105710f9b6dbffff74a03fe15e860d4
raven404 <gky1232000@gmail.com>
2019-07-02 03:57:03 -0400

    function definationadded for the movement of the snake during the play

--- snake.py
+++ snake.py
@@ -1,14 +1,14 @@
 from tkinter import *
 import random
 
-# Globals
+# Global variables used
 WIDTH = 800
 HEIGHT = 600
 SEG_SIZE = 20
 IN_GAME = True
 
 
-# Helper functions
+# Function used for creating prey for the snake
 def create_block():
     """ Creates an apple to be eaten """
     global BLOCK
@@ -17,7 +17,7 @@ def create_block():
     BLOCK = c.create_oval(posx, posy,
                           posx+SEG_SIZE, posy+SEG_SIZE,
                           fill="red")
-
+# main function to control the game
 def main():
     """ Handles game process """
     global IN_GAME
@@ -49,15 +49,16 @@ def main():
                       font="Arial 20",
                       fill="red")
 
-
+# class which determine the snakes formation th roughout the game and segment
 class Segment(object):
     """ Single snake segment """
     def __init__(self, x, y):
         self.instance = c.create_rectangle(x, y,
                                            x+SEG_SIZE, y+SEG_SIZE,
                                            fill="white")
+# class ehich holds functions for determining the mmovement of the snake throughout the game
 class Snake(object):
-    """ Simple Snake class """
+    """ initializing the snakes movement """
     def __init__(self, segments):
         self.segments = segments
         # possible moves
@@ -65,3 +66,15 @@ class Snake(object):
                         "Up": (0, -1), "Left": (-1, 0)}
         # initial movement direction
         self.vector = self.mapping["Right"]
+        # defination for moving the snake automatically
+    def move(self):
+        """ Moves the snake with the specified vector"""
+        for index in range(len(self.segments)-1):
+            segment = self.segments[index].instance
+            x1, y1, x2, y2 = c.coords(self.segments[index+1].instance)
+            c.coords(segment, x1, y1, x2, y2)
+
+        x1, y1, x2, y2 = c.coords(self.segments[-2].instance)
+        c.coords(self.segments[-1].instance,
+                 x1+self.vector[0]*SEG_SIZE, y1+self.vector[1]*SEG_SIZE,
+                 x2+self.vector[0]*SEG_SIZE, y2+self.vector[1]*SEG_SIZE)
